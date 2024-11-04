03305734603d93d4eaf11fa2fd63194159779ea7
tusharpandey151 <tusharpandey151@gmail.com>
2024-11-04 15:56:15 +0000

    Enhanced Multithreaded Config Manager for singleton Pattern

--- src/main/java/designpatterns/creational/singleton/TestDriver.java
+++ src/main/java/designpatterns/creational/singleton/TestDriver.java
@@ -54,6 +54,51 @@ class ConfigurationManagerEnhanced {
     }
 }
 
+class ConfigurationManagerMultithreadedEnhanced {
+
+    /*Volatile on Config Manager makes sure of two things
+    1. It prevents Instruction reordering (possible for java Compiler to enhance performance -- where instantiation, memory allocation and initialization steps could be reorganised and a thread could try 
+    to access an incomplete instance of Config Manger) - Volatile Prevents this reordering 
+    2. It makes sure that no thread caches the value of the variable and the latest value is always visible to all threads 
+    What it does not make sure is - 
+
+    1 It does not guarantee atomicity - It will not make sure that this variable is not accessed by two threads at the same time
+    */
+    private static volatile ConfigurationManagerMultithreadedEnhanced configurationManagerMultithreadedEnhanced;  
+    private Map<String, String> config;
+
+    //So no one can do any instantiation of ConfigurationManager
+    private ConfigurationManagerMultithreadedEnhanced() { 
+        loadConfig();
+    }
+
+    public static ConfigurationManagerMultithreadedEnhanced getConfigurationManagerMultithreadedEnhanced() {
+        if(Objects.isNull(configurationManagerMultithreadedEnhanced)) {
+            synchronized (ConfigurationManagerMultithreadedEnhanced.class) { // this here makes sure that two threads cannot simultaneously try to instantiate instance of Config Manager
+                if(Objects.isNull(configurationManagerMultithreadedEnhanced)) {
+                    configurationManagerMultithreadedEnhanced = new ConfigurationManagerMultithreadedEnhanced();
+                }
+            }
+        } 
+        return configurationManagerMultithreadedEnhanced;
+
+    }
+
+    public synchronized String getConfig(String key) { // making both set and get synchronized makes sure that one thread when trying to get .. will not allow another to set and vice versa
+        return this.config.get(key);
+    }
+
+    public synchronized String setConfig(String key, String value) {
+        return this.config.put(key, value);
+    }
+
+    private void loadConfig() {
+        this.config = new HashMap<>();
+        this.config.put("url", "someURL"+Math.random());
+        this.config.put("apiKey", "someApiKey"+Math.random());
+    }
+}
+
 
 
 
